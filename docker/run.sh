f54a6714658ed25a108f2c71b9a8a71260384029
Minjun Xu <mjxu96@gmail.com>
2020-01-31 13:29:35 -0500

    update Dockerfile to only include backend and frontend

--- docker/run.sh
+++ docker/run.sh
@@ -1,18 +1,10 @@
-
-echo "Launching carla-simulator"
-./carla-simulator/CarlaUE4.sh &
-sleep 3
-echo "carla-simulator launched."
-
 echo "Launching backend"
 ./carla-display-backend/bin/platform &
 sleep 3
 echo "backend launched."
-sleep infinity
 
 echo "Launching frontend"
 cd ./carla-display-frontend/
 yarn start-live &
-sleep 10
 echo "frontend launched. Please open your browser"
 sleep infinity
