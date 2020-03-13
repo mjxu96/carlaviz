2be28b6bf4f48272e92782dc0137761a0998a820
Minjun Xu <mjxu96@gmail.com>
2020-03-13 12:12:21 -0400

    add frontend files

---
+++ frontend/src/app.js
@@ -0,0 +1,165 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+/* global document, console */
+/* eslint-disable no-console, no-unused-vars, no-undef */
+import React, { PureComponent } from "react";
+import { render } from "react-dom";
+
+import { GeoJsonLayer } from "@deck.gl/layers";
+import { COORDINATE_SYSTEM } from "@deck.gl/core";
+import {
+  LogViewer,
+  StreamSettingsPanel,
+  MeterWidget,
+  XVIZPanel,
+  VIEW_MODE
+} from "streetscape.gl";
+import { Form, ThemeProvider } from "@streetscape.gl/monochrome";
+
+import {
+  APP_SETTINGS,
+  MAPBOX_TOKEN,
+  MAP_STYLE,
+  XVIZ_STYLE,
+  CAR,
+  UI_THEME
+} from "./constants";
+
+const exampleLog = require("./log-from-live").default;
+
+const tableComponentProps = {
+  table: {
+    height: 120
+  }
+};
+class Example extends PureComponent {
+  state = {
+    log: exampleLog,
+    settings: {
+      viewMode: "PERSPECTIVE",
+      showTooltip: true
+    }
+  };
+
+  componentDidMount() {
+    const { log } = this.state;
+    log
+      .on("ready", () => {
+        const metadata = log.getMetadata();
+        if (metadata.map) {
+          const mapLayer = new GeoJsonLayer({
+            coordinateSystem: COORDINATE_SYSTEM.METER_OFFSETS,
+            coordinateOrigin: [0, 0, 0],
+            id: "carla_map",
+            data: metadata.map,
+            stroked: true,
+            filled: true,
+            wireframe: true,
+            extruded: true,
+            getFillColor: [255, 255, 255, 255],
+            getLineColor: [255, 255, 255, 255],
+            getLineWidth: 0.1,
+            getRadius: 0.00001,
+            opacity: 10
+          });
+          this.setState({ map: mapLayer });
+          console.log("get map");
+        } else {
+          console.log("receive metadata without map");
+        }
+      })
+      .on("error", console.error)
+      .connect();
+  }
+
+  _onSettingsChange = changedSettings => {
+    this.setState({
+      settings: { ...this.state.settings, ...changedSettings }
+    });
+  };
+
+  render() {
+    const { log, map, settings } = this.state;
+    const customLayers = [map];
+
+    return (
+      <div id="container">
+        <div id="control-panel">
+          <XVIZPanel log={log} name="Metrics" />
+          <hr />
+          <XVIZPanel log={log} name="Camera" />
+          <hr />
+          <XVIZPanel
+            log={log}
+            name="Tables"
+            componentProps={tableComponentProps}
+          />
+          <hr />
+          <Form
+            data={APP_SETTINGS}
+            values={this.state.settings}
+            onChange={this._onSettingsChange}
+          />
+          <StreamSettingsPanel log={log} />
+        </div>
+        <div id="log-panel">
+          <div id="map-view">
+            <LogViewer
+              log={log}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+              mapStyle={MAP_STYLE}
+              car={CAR}
+              xvizStyles={XVIZ_STYLE}
+              showTooltip={settings.showTooltip}
+              viewMode={VIEW_MODE[settings.viewMode]}
+              customLayers={customLayers}
+            />
+            <div id="hud">
+              <MeterWidget
+                log={log}
+                streamName="/vehicle/acceleration"
+                label="Acceleration"
+                min={-10}
+                max={10}
+              />
+              <hr />
+              <MeterWidget
+                log={log}
+                streamName="/vehicle/velocity"
+                label="Speed"
+                getWarning={x => (x > 6 ? "FAST" : "")}
+                min={0}
+                max={20}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(
+  <ThemeProvider theme={UI_THEME}>
+    <Example />
+  </ThemeProvider>,
+  document.getElementById("app")
+);
---
+++ frontend/src/constants.js
@@ -0,0 +1,82 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+import { CarMesh } from "streetscape.gl";
+
+//import {load} from '@loaders.gl/core';
+//import {OBJLoader} from '@loaders.gl/obj';
+
+export const UI_THEME = {
+  extends: "dark",
+  background: "rgba(51,51,51,0.9)",
+  backgroundAlt: "#222222",
+
+  controlColorPrimary: "#858586",
+  controlColorSecondary: "#636364",
+  controlColorDisabled: "#404042",
+  controlColorHovered: "#F8F8F9",
+  controlColorActive: "#5B91F4",
+
+  textColorPrimary: "#F8F8F9",
+  textColorSecondary: "#D0D0D1",
+  textColorDisabled: "#717172",
+  textColorInvert: "#1B1B1C",
+
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Arial, sans-serif',
+  fontSize: 14,
+  fontWeight: 200,
+
+  shadow: "0 2px 4px 0 rgba(0, 0, 0, 0.15)"
+};
+
+/* eslint-disable camelcase */
+export const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+export const MAP_STYLE = "mapbox://styles/mapbox/dark-v10";
+
+export const CAR = CarMesh.sedan({
+  origin: [1.08, 0, 0],
+  length: 4.7,
+  width: 2.5,
+  height: 1.7,
+  color: [128, 128, 128]
+});
+
+export const APP_SETTINGS = {
+  viewMode: {
+    type: "select",
+    title: "View Mode",
+    data: { TOP_DOWN: "Top Down", PERSPECTIVE: "Perspective", DRIVER: "Driver" }
+  },
+  showTooltip: {
+    type: "toggle",
+    title: "Show Tooltip"
+  }
+};
+
+export const XVIZ_STYLE = {
+  "/object/vehicles": [
+    { name: "selected", style: { fill_color: "#ff8000aa" } }
+  ],
+  "/object/walkers": [
+    { name: "selected", style: { fill_color: "#ff8000aa" } }
+  ],
+  "/lidar/points": [{ style: { point_color_mode: "ELEVATION" } }]
+};
