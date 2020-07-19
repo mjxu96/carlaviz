5118526f7b7faf8be77401a9485dbf596625816f
Minjun Xu <mjxu96@gmail.com>
2020-07-19 18:19:45 +0800

    upadte nodejs version to 12.x

--- backend/setup/download.sh
+++ backend/setup/download.sh
@@ -1,6 +1,6 @@
 
-# install nodejs 10.x
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+# install nodejs 12.x
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install yarn
