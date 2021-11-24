5f1bdfe88aa2aa6b66fa0cc2029e15e265cbe0f5
umateusz <umateusz@op.pl>
2021-11-24 03:38:58 +0100

    Upgrade to 0.9.12 (#44)
    upgrade to 0.9.12

--- backend/third_party/LibCarla/source/carla/geom/Vector3DInt.h
+++ backend/third_party/LibCarla/source/carla/geom/Vector3DInt.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// Copyright (c) 2021 Computer Vision Center (CVC) at the Universitat Autonoma
 // de Barcelona (UAB).
 //
 // This work is licensed under the terms of the MIT license.
@@ -14,26 +14,26 @@
 namespace carla {
 namespace geom {
 
-  class Vector3D {
+  class Vector3DInt {
   public:
 
     // =========================================================================
     // -- Public data members --------------------------------------------------
     // =========================================================================
 
-    float x = 0.0f;
+    int32_t x = 0;
 
-    float y = 0.0f;
+    int32_t y = 0;
 
-    float z = 0.0f;
+    int32_t z = 0;
 
     // =========================================================================
     // -- Constructors ---------------------------------------------------------
     // =========================================================================
 
-    Vector3D() = default;
+    Vector3DInt() = default;
 
-    Vector3D(float ix, float iy, float iz)
+    Vector3DInt(int32_t ix, int32_t iy, int32_t iz)
       : x(ix),
         y(iy),
         z(iz) {}
@@ -42,89 +42,72 @@ namespace geom {
     // -- Other methods --------------------------------------------------------
     // =========================================================================
 
-    float SquaredLength() const {
+    int64_t SquaredLength() const {
       return x * x + y * y + z * z;
     }
 
-    float Length() const {
+    double Length() const {
        return std::sqrt(SquaredLength());
-    }
-
-    Vector3D Abs() const {
-       return Vector3D(abs(x), abs(y), abs(z));
-    }
-
-    Vector3D MakeUnitVector() const {
-      const float length = Length();
-      DEVELOPMENT_ASSERT(length > 2.0f * std::numeric_limits<float>::epsilon());
-      const float k = 1.0f / length;
-      return Vector3D(x * k, y * k, z * k);
-    }
-
-    Vector3D MakeSafeUnitVector(const float epsilon) const  {
-      const float length = Length();
-      const float k = (length > std::max(epsilon, 0.0f)) ? (1.0f / length) : 1.0f;
-      return Vector3D(x * k, y * k, z * k);
     }
 
     // =========================================================================
     // -- Arithmetic operators -------------------------------------------------
     // =========================================================================
 
-    Vector3D &operator+=(const Vector3D &rhs) {
+    Vector3DInt &operator+=(const Vector3DInt &rhs) {
       x += rhs.x;
       y += rhs.y;
       z += rhs.z;
       return *this;
     }
 
-    friend Vector3D operator+(Vector3D lhs, const Vector3D &rhs) {
+    friend Vector3DInt operator+(Vector3DInt lhs, const Vector3DInt &rhs) {
       lhs += rhs;
       return lhs;
     }
 
-    Vector3D &operator-=(const Vector3D &rhs) {
+    Vector3DInt &operator-=(const Vector3DInt &rhs) {
       x -= rhs.x;
       y -= rhs.y;
       z -= rhs.z;
       return *this;
     }
 
-    friend Vector3D operator-(Vector3D lhs, const Vector3D &rhs) {
+    friend Vector3DInt operator-(Vector3DInt lhs, const Vector3DInt &rhs) {
       lhs -= rhs;
       return lhs;
     }
 
-    Vector3D &operator*=(float rhs) {
+    Vector3DInt &operator*=(int32_t rhs) {
       x *= rhs;
       y *= rhs;
       z *= rhs;
       return *this;
     }
 
-    friend Vector3D operator*(Vector3D lhs, float rhs) {
+    friend Vector3DInt operator*(Vector3DInt lhs, int32_t rhs) {
       lhs *= rhs;
       return lhs;
     }
 
-    friend Vector3D operator*(float lhs, Vector3D rhs) {
+    friend Vector3DInt operator*(int32_t lhs, Vector3DInt rhs) {
       rhs *= lhs;
       return rhs;
     }
 
-    Vector3D &operator/=(float rhs) {
+    Vector3DInt &operator/=(int32_t rhs) {
       x /= rhs;
       y /= rhs;
       z /= rhs;
       return *this;
     }
 
-    friend Vector3D operator/(Vector3D lhs, float rhs) {
+    friend Vector3DInt operator/(Vector3DInt lhs, int32_t rhs) {
       lhs /= rhs;
       return lhs;
     }
 
-    friend Vector3D operator/(float lhs, Vector3D rhs) {
+    friend Vector3DInt operator/(int32_t lhs, Vector3DInt rhs) {
       rhs /= lhs;
       return rhs;
     }
@@ -133,11 +116,11 @@ namespace geom {
     // -- Comparison operators -------------------------------------------------
     // =========================================================================
 
-    bool operator==(const Vector3D &rhs) const {
+    bool operator==(const Vector3DInt &rhs) const {
       return (x == rhs.x) && (y == rhs.y) && (z == rhs.z);
     }
 
-    bool operator!=(const Vector3D &rhs) const {
+    bool operator!=(const Vector3DInt &rhs) const {
       return !(*this == rhs);
     }
 
@@ -147,24 +130,21 @@ namespace geom {
 
 #ifdef LIBCARLA_INCLUDED_FROM_UE4
 
-    /// These 2 methods are explicitly deleted to avoid creating them by other users,
-    /// unlike locations, some vectors have units and some don't, by removing
-    /// these methods we found several places were the conversion from cm to m was missing
-    Vector3D(const FVector &v) = delete;
-    Vector3D& operator=(const FVector &rhs) = delete;
+    Vector3DInt(const FIntVector &v) = delete;
+    Vector3DInt& operator=(const FIntVector &rhs) = delete;
 
-    /// Return a Vector3D converted from centimeters to meters.
-    Vector3D ToMeters() const {
-      return *this * 1e-2f;
+    /// Return a Vector3DInt converted from centimeters to meters.
+    Vector3DInt ToMeters() const {
+      return *this / 100;
     }
 
-    /// Return a Vector3D converted from meters to centimeters.
-    Vector3D ToCentimeters() const {
-      return *this * 1e2f;
+    /// Return a Vector3DInt converted from meters to centimeters.
+    Vector3DInt ToCentimeters() const {
+      return *this * 100;
     }
 
-    FVector ToFVector() const {
-      return FVector{x, y, z};
+    FIntVector ToFIntVector() const {
+      return FIntVector{x, y, z};
     }
 
 #endif // LIBCARLA_INCLUDED_FROM_UE4
